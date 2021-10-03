41b5d47dba88f5679a87c7918c018871a1d2d8b3
KorbsStudio <ks@korbsstudio.com>
2021-10-02 20:55:53 -0400

    Minify

--- src/js/electron/preload.js
+++ src/js/electron/preload.js
@@ -1,40 +1,27 @@
 const { contextBridge, ipcRenderer} = require("electron");
 
-contextBridge.exposeInMainWorld(
-  "api", {
-      send: (channel, data) => {
-          let validChannels = ["minimize",
-                               "maximize",
-                               "restore",
-                               "close",
-                               "update",
-                               "open-update-dialog",
-                               "open-sample-dialog"];
-          if (validChannels.includes(channel)) {
-              ipcRenderer.send(channel, data);
-          }
-      }
-  }
-);
+contextBridge.exposeInMainWorld( "api", { send: (channel, data) => {let validChannels = [
+  "minimize",
+  "maximize",
+  "restore",
+  "close",
+  "update",
+  "open-update-dialog",
+  "open-failed-dialog",
+  "open-sample-dialog"
+]; if (validChannels.includes(channel)) {ipcRenderer.send(channel, data);}}});
 
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+window.addEventListener('DOMContentLoaded', () => {const replaceText = (selector, text) => {const element = document.getElementById(selector)
+if (element) element.innerText = text}
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
-
+for (const type of ['chrome', 'node', 'electron']) {replaceText(`${type}-version`, process.versions[type])}})
 setTimeout(function(){
   document.getElementById("version").innerHTML = process.env.npm_package_version;
   document.getElementById("electron-version").innerHTML = process.versions.electron;
   document.getElementById("electron-p-version").innerHTML = process.versions.electron;
   document.getElementById("node-version").innerHTML = process.versions.node;
   document.getElementById("chrome-version").innerHTML = process.versions.chrome;
-}, 2500);
+}, 275);
 
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
